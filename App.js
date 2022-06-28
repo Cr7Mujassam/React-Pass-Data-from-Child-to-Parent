b68792c76c9813b424cc8127106ae973f62719dd
Cr7Mujassam <106069072+Cr7Mujassam@users.noreply.github.com>
2022-06-28 15:43:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,7 +7,8 @@ function App() {
   function getname(name){
     alert(name);
   }
-
+  
+ // We need to make the funtion to pass the data like here we make the getname function
   return (
     <div className="App">
       <h1>Transfering the data from child to Parent Components</h1>
